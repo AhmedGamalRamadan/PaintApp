dc61256878ee7db18c772b8b9e7f012b7f0d40d2
Paint <AHMED00AHMED00GAMAL@gmail.com>
2023-09-20 21:03:45 +0200

    First Commit

---
+++ app/src/main/java/com/example/android/paint/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.example.android.paint
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View.SYSTEM_UI_FLAG_FULLSCREEN
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val myCanvasView = MyCanvasView(this)
+        myCanvasView.systemUiVisibility = SYSTEM_UI_FLAG_FULLSCREEN
+        myCanvasView.contentDescription = getString(R.string.canvasContentDescription)
+        setContentView(myCanvasView)
+    }
+}
---
+++ app/src/main/java/com/example/android/paint/MyCanvasView.kt
@@ -0,0 +1,102 @@
+package com.example.android.paint
+
+import android.content.Context
+import android.graphics.*
+import android.view.MotionEvent
+import android.view.View
+import android.view.ViewConfiguration
+import androidx.core.content.res.ResourcesCompat
+
+private const val STROKE_WIDTH = 12f
+
+class MyCanvasView(context: Context) : View(context) {
+    private lateinit var extraCanvas: Canvas
+    private lateinit var extraBitmap: Bitmap
+
+    //background Color of Screen
+    private val backgroundColor = ResourcesCompat.getColor(resources, R.color.colorBackground, null)
+    //Draw Color
+    private val drawColor = ResourcesCompat.getColor(resources, R.color.colorPaint, null)
+    private val dColor=ResourcesCompat.getColor(resources,R.color.black,null)
+
+
+    private val paint = Paint().apply {
+        color = drawColor
+        isAntiAlias = true
+
+        isDither = true
+        style = Paint.Style.STROKE
+        strokeJoin = Paint.Join.ROUND
+        strokeCap = Paint.Cap.ROUND
+        strokeWidth = STROKE_WIDTH
+    }
+    private var path = Path()
+
+    private var motionTouchEventX = 0f
+    private var motionTouchEventY = 0f
+
+    private var currentX = 0f
+    private var currentY = 0f
+
+    private val touchTolerance = ViewConfiguration.get(context).scaledTouchSlop
+
+    //to add frame
+    private lateinit var frame: Rect
+
+    override fun onSizeChanged(width: Int, height: Int, oldWidth: Int, oldHeight: Int) {
+        super.onSizeChanged(width, height, oldWidth, oldHeight)
+
+        if (::extraBitmap.isInitialized) extraBitmap.recycle()
+        extraBitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888)
+        extraCanvas = Canvas(extraBitmap)
+        extraCanvas.drawColor(backgroundColor)
+        //to add frame
+
+        val inset = 40
+        frame = Rect(inset, inset, width - inset, height - inset)
+    }
+
+    override fun onDraw(canvas: Canvas) {
+        super.onDraw(canvas)
+        canvas.drawBitmap(extraBitmap, 0f, 0f, null)
+        // Draw a frame around the canvas.
+        canvas.drawRect(frame, paint)
+    }
+    private fun touchStart() {
+        path.reset()
+        path.moveTo(motionTouchEventX, motionTouchEventY)
+        currentX = motionTouchEventX
+        currentY = motionTouchEventY
+    }
+
+    private fun touchMove() {
+        val dx = Math.abs(motionTouchEventX - currentX)
+        val dy = Math.abs(motionTouchEventY - currentY)
+        if (dx >= touchTolerance || dy >= touchTolerance) {
+
+            path.quadTo(currentX, currentY, (motionTouchEventX + currentX) / 2, (motionTouchEventY + currentY) / 2)
+            currentX = motionTouchEventX
+            currentY = motionTouchEventY
+            // Draw the path in the extra bitmap to cache it.
+            extraCanvas.drawPath(path, paint)
+        }
+        invalidate()
+    }
+
+    private fun touchUp() {
+        // Reset the path
+        path.reset()
+    }
+    override fun onTouchEvent(event: MotionEvent): Boolean {
+        motionTouchEventX = event.x
+        motionTouchEventY = event.y
+
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> touchStart()
+            MotionEvent.ACTION_MOVE -> touchMove()
+            MotionEvent.ACTION_UP -> touchUp()
+        }
+        return true
+    }
+
+}
